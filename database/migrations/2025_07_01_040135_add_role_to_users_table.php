33cfdd983515b5868668e237e5203ff6b973b9f1
forujjust@gmail.com <forujjust@gmail.com>
2025-07-08 15:05:38 +0700

    dropdown menu

--- database/migrations/2025_07_01_040135_add_role_to_users_table.php
+++ database/migrations/2025_07_01_040135_add_role_to_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             // Menambahkan kolom 'role' setelah kolom 'password'
-            $table->string('role')->default('user')->after('password');
+            $table->string('role')->default('anggota')->after('password');
         });
     }
 
